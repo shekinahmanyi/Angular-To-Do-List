57aee6d401a92ec1b8dc44d03fecdd094885b7a9
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-21 17:31:34 +0100

    Installed Moongoose and MongoDB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();  
 
-app.get('/', (req, res) => {  
+/* Route Handlers*/
+
+/* List Routes*/
+
+/* Get Lists*/
+app.get('/lists', (req, res) => {  
+//Return an array of lists
     res.send('Hello World');
+})
+
+/* Post List*/
+app.post('/lists', (req, res) => {
+    //create new list and return the new list document back to the user (which includes the id)
+    //the list information (fields) will be passed in via the JSON request body
+})
+
+/* Patch List*/
+app.patch('/lists/:id', (req, res) => {
+    //update specified list (list document with id in the URL) with new values specified in the JSON body of the request
+})
+
+/* Delete List*/
+app.delete('/lists/:id', (req, res) => {
+    //delete specified list (document with id in the URL)
 })
 
 app.listen(3000, () => {
