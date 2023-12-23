8602f0b299bbaef085e314e26087e3d0a3df3cb1
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-23 11:10:41 +0100

    Add HttpClientModule import to app.component.ts

--- to-do-list/src/app/app.component.ts
+++ to-do-list/src/app/app.component.ts
@@ -3,14 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';  
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 
+import { HttpClientModule, HttpClient} from '@angular/common/http';
+
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, TaskViewComponent], 
+  imports: [CommonModule, RouterModule, TaskViewComponent,HttpClientModule], 
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],  
 })
+
+
 export class AppComponent {
   title = 'to-do-list';
 }
