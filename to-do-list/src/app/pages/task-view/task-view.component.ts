2fe55e90f54b5f3bb4c24425ea5f60f9e7b4ce17
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-23 11:11:23 +0100

    Add TaskService and HttpClientModule imports, and update createNewList method

--- to-do-list/src/app/pages/task-view/task-view.component.ts
+++ to-do-list/src/app/pages/task-view/task-view.component.ts
@@ -1,22 +1,28 @@
+import { TaskService } from './../../task.service';
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-task-view',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './task-view.component.html',
   styleUrl: './task-view.component.css'
 })
 
 export class TaskViewComponent {
 
-  constructor() {}
+  constructor(private TaskService: TaskService) {}
 
   ngOnInit() {
 
   }
 
-  createNewList(title: string) {
+  createNewList() {
+    this.TaskService.createList('Testing New List').subscribe((response: any) => {
+      console.log(response);
+    });
  
   }
 
