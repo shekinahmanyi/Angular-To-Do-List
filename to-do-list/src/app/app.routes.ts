52761ca153d0caa29dedf5a6886a95fa8344397a
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-23 12:35:46 +0100

    Add routes for TaskViewComponent and NewListComponent

--- to-do-list/src/app/app.routes.ts
+++ to-do-list/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TaskViewComponent },
+    { path: 'new-list', component: NewListComponent },
+];
