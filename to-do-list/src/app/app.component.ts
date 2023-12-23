ac044e8d2345df3de2f6398036828a4bbeb9e23f
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-23 12:35:33 +0100

    Add NewListComponent to app imports

--- to-do-list/src/app/app.component.ts
+++ to-do-list/src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';  
 import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
 
-import { HttpClientModule, HttpClient} from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, TaskViewComponent,HttpClientModule], 
+  imports: [CommonModule, RouterModule, TaskViewComponent,HttpClientModule,NewListComponent], 
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],  
