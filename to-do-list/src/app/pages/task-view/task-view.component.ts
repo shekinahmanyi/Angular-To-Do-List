79a95a0b55b04cc2964645e295329ccb577e839c
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-24 01:19:40 +0100

    Add ActivatedRoute and TaskService dependencies to TaskViewComponent

--- to-do-list/src/app/pages/task-view/task-view.component.ts
+++ to-do-list/src/app/pages/task-view/task-view.component.ts
@@ -1,7 +1,7 @@
 import { TaskService } from './../../task.service';
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 
 @Component({
@@ -14,13 +14,25 @@ import { Router } from '@angular/router';
 
 export class TaskViewComponent {
 
-  constructor(private router:Router) {}
+  lists:any;
+
+  constructor(private router:Router, private taskService:TaskService, private route:ActivatedRoute ) {}
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params:Params) => {
+        console.log(params);
+      }
+    );
 
+    this.taskService.getLists().subscribe((lists:any) => {
+      // Code block inside the subscription
+      this.lists = lists;
+    });
   }
+
   onNewListClick() {
     this.router.navigate(['/new-list']);
   }
-
+  
 }
