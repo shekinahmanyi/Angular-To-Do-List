19cd83460d1e254bea88fd289ca2ed69e99edc67
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-24 13:26:18 +0100

    Add imports and update component logic in task-view.component.ts

--- to-do-list/src/app/pages/task-view/task-view.component.ts
+++ to-do-list/src/app/pages/task-view/task-view.component.ts
@@ -2,12 +2,15 @@ import { TaskService } from './../../task.service';
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 
 @Component({
   selector: 'app-task-view',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, RouterLinkActive,RouterLink,NgFor],
   templateUrl: './task-view.component.html',
   styleUrl: './task-view.component.css'
 })
@@ -15,14 +18,20 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 export class TaskViewComponent {
 
   lists:any;
+  tasks:any;
 
   constructor(private router:Router, private taskService:TaskService, private route:ActivatedRoute ) {}
 
   ngOnInit() {
     this.route.params.subscribe(
       (params:Params) => {
-        console.log(params);
+        console.log(params);   
+
+        this.taskService.getTasks(params['listId']).subscribe((tasks) => {
+          this.tasks = tasks as any;
+        });
       }
+       
     );
 
     this.taskService.getLists().subscribe((lists:any) => {
@@ -35,4 +44,7 @@ export class TaskViewComponent {
     this.router.navigate(['/new-list']);
   }
   
+  onListId(list: any) {
+    this.router.navigate([`/lists/${list._id}`]);
+  }
 }
