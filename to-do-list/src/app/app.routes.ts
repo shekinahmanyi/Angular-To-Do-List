33ed642dd58c3eea4b8f5efcca0a2e5cbc2b8ca0
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-24 01:17:38 +0100

    Update app routes to redirect to 'lists' path

--- to-do-list/src/app/app.routes.ts
+++ to-do-list/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NewListComponent } from './pages/new-list/new-list.component';
 
 export const routes: Routes = [
-    { path: '', component: TaskViewComponent },
+    { path: '', redirectTo: 'lists', pathMatch: 'full' },
     { path: 'new-list', component: NewListComponent },
+    { path: 'lists', component: TaskViewComponent },
+  { path: 'lists/:listId', component: TaskViewComponent },
 ];
